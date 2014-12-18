96649733723b7084d076ee700fe4e8cd8b624298
Jeroen Jacobs <git@jeroenj.be>
2014-12-18 22:32:12 +0100

    Prepares for v2.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Jeroen Jacobs'
 maintainer_email  'chef@jeroenj.be'
 license           'MIT'
 description       'Installs PlexConnect.'
-version           '2.0.0'
+version           '2.1.0'
 
 depends           'python'
 
