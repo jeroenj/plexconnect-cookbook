ad18336bff6ecee116502c1dfb9ae3b2c5ae2dcd
Jeroen Jacobs <git@jeroenj.be>
2015-01-13 21:59:57 +0100

    Bumps version to v2.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Jeroen Jacobs'
 maintainer_email  'chef@jeroenj.be'
 license           'MIT'
 description       'Installs PlexConnect.'
-version           '2.1.1'
+version           '2.1.2'
 
 depends           'git'
 depends           'python'
